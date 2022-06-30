8ecdc5d47a227dea7a2bc5a9a0cbc40e6282ca19
Liam Doran <65523663+Minerbot360@users.noreply.github.com>
2022-06-29 22:46:06 -0700

    Day 20, project 2 - 1

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Liam Doran on 6/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Liam Doran on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
