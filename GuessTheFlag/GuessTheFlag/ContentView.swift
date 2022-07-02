79b33ff23999fc48bd3a7cd603ae0fd3d211ff34
Liam Doran <65523663+Minerbot360@users.noreply.github.com>
2022-07-01 22:33:58 -0700

    day 22, project 2 - 3
    I cannot count, yesterday's was supposed to say 2 - 2 not 3 - 2.

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showingScore = false
+    @State private var showingGameOver = false
+    
     @State private var scoreTitle = ""
+    @State private var score: Int = 0
+    
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var questions = 0
+    @State private var flagTapped = 1
     
     var body: some View {
         ZStack {
@@ -34,9 +41,9 @@ struct ContentView: View {
                             .foregroundStyle(.secondary)
                             .font(.subheadline.weight(.heavy))
                         Text(countries[correctAnswer])
-                            .foregroundStyle(.secondary)
                             .font(.largeTitle.weight(.semibold))
                     }
+                    
                     ForEach(0..<3) { number in
                         Button {
                            flagTapped(number)
@@ -54,28 +61,40 @@ struct ContentView: View {
                 .clipShape(RoundedRectangle(cornerRadius: 20))
                 
                 Spacer()
+                Spacer()
                 
-                Text("Score: ???")
+                Text("Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                
+                Text("Question: \(questions)/8")
                     .foregroundColor(.white)
                     .font(.title.bold())
                 
                 Spacer()
-                Spacer()
             }
-            
-            
+            .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(score)")
+        }
+        
+        .alert("Game over", isPresented: $showingGameOver) {
+            Button("Reset", action: resetGame)
+        } message: {
+            Text("You finished with a final score of \(score)!")
         }
     }
+    
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            score += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong, you picked \(countries[number])"
+            score -= 1
         }
 
         showingScore = true
@@ -84,6 +103,19 @@ struct ContentView: View {
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        
+        questions += 1
+        
+        if questions < 8 {
+            return
+        } else if questions == 8 {
+            showingGameOver = true
+        }
+    }
+    
+    func resetGame() {
+        questions = 0
+        score = 0
     }
 }
 
