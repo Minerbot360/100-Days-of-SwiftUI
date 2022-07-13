9d53cd1ef87bf9c7250b5c5449b9bf878d3d88ab
Liam Doran <65523663+Minerbot360@users.noreply.github.com>
2022-07-12 22:22:46 -0700

    day 32, project 6 - 1

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Liam Doran on 7/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
